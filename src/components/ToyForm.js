6ef18ae3e1e7928598e5f83601f50504f48540ac
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-05-17 13:17:11 +0300

    commit tree a34c1e9be1f22b7d6b83fb4d56cc5c650bec4ff3

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({onNewToy}) {
+  const [formData, setFomData] = useState({
+    name: "",
+    image: "",
+    likes: 0
+  })
+
+  function handleChange(e){
+    setFomData({...formData, [e.target.name]:e.target.value})
+  }
+  console.log(formData);
+  function handleSubmit(e){
+    e.preventDefault()
+    const action = async()=>{
+      const data = await fetch('http://localhost:3001/toys', {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(formData)
+
+      })
+      const resp=  data.json()
+      onNewToy(resp)
+    }
+    action()
+    // fetch('http://localhost:3001/toys', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type':'application/json'
+    //     },
+    //     body: JSON.stringify(formData)
+    //   })
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value= {formData.name}
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +52,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value= {formData.image}
+          onChange={handleChange}
         />
         <br />
         <input
